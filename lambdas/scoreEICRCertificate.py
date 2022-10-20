441c13ba11df8500cbba280786eb4e6e02742f4c
Colin B <colinbowen2@gmail.com>
2022-10-20 16:32:04 +0100

    Remove unused lambda

--- lambdas/scoreEICRCertificate.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    print("Event", event)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
